0a2445105c5b35d8d2976339923a5eb3d0fcd15a
JBallou-1 <54325784+JBallou-1@users.noreply.github.com>
2021-03-09 23:43:00 -0500

    Preliminary homework upload

--- app/src/main/java/com/example/tipcalculator_ballou/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator_ballou/MainActivity.java
@@ -1,26 +1,32 @@
 package com.example.tipcalculator_ballou;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
-
+    //declare edittexts, radioButtons, and buttons
+    private EditText payment;
+    private EditText people;
+    private EditText customTipAmount;
+    private RadioGroup tipamounts;
+    private RadioButton selected;
+    private Button reset;
+    private Button calculate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final ListView menu = findViewById(R.id.Mainmenu);
-        menu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                
-            }
-        });
+        payment = (EditText) this.findViewById(R.id.payment);
+        people = (EditText) this.findViewById(R.id.people);
+        customTipAmount = (EditText) this.findViewById(R.id.customTipAmount);
+        tipamounts = (RadioGroup) this.findViewById(R.id.tipamounts);
+        reset = (Button) this.findViewById(R.id.reset);
+        calculate = (Button) this.findViewById(R.id.calculate);
+        //have tipamounts set a listener that, when clicked, calls getCheckedRadioButtonID() and do things depending on what that returns
     }
 }
